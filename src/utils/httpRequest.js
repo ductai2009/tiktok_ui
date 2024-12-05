07fa4b238ca4fe440c34bcdcca789155741bfe68
DucTai <ductai200920@gmail.com>
2024-12-05 16:55:33 +0700

    update call api

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const request = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+    // baseURL: process.env.REACT_APP_BASE_URL,
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
 });
